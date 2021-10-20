f59aeee5c4b957a69594e81bef9d46cb7e8b4a8c
goz310103 <tamiguisimo@gmail.com>
2021-10-19 22:47:17 -0600

    server proyect main java class postfix form was evaluated in a good way

---
+++ MyServer/src/expressiontree/Main234.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expressiontree;
+import java.util.Stack;
+/**
+ *
+ * @author jose
+ */
+public class Main234 {
+   // Function to evaluate a given postfix expression
+   public static int evalPostfix(String[] exp)
+   {
+      // base case
+      if (exp == null || exp.length == 0) {
+         System.exit(-1);
+      }
+
+      // create an empty stack
+      Stack<Integer> stack = new Stack<>();
+
+      // traverse the given expression
+      for (int i = 0; i< exp.length;i++)
+      {
+         // if the current character is an operand, push it into the stack
+         if (isNumeric(exp[i])) {
+
+            stack.push(Integer.parseInt(exp[i]));
+         }
+         // if the current character is an operator
+         else {
+            // remove the top two elements from the stack
+
+            int x = stack.pop();
+            int y = stack.pop();
+            //String y = stack.pop();
+
+            // evaluate the expression 'x op y', and push the
+            // result back to the stack
+            if (exp[i].contains("+")) {
+               stack.push(y + x);
+            }
+            else if (exp[i].contains("-")) {
+               stack.push(y - x);
+            }
+            else if (exp[i].contains("*")) {
+               stack.push(y * x);
+            }
+            else if (exp[i].contains("/")) {
+               stack.push(y / x);
+            }
+         }
+      }
+
+      // At this point, the stack is left with only one element, i.e.,
+      // expression result
+      return stack.pop();
+   }
+
+   public static boolean isNumeric(String strNum) {
+      if (strNum == null) {
+         return false;
+      }
+      try {
+         double d = Double.parseDouble(strNum);
+      } catch (NumberFormatException nfe) {
+         return false;
+      }
+      return true;
+   }
+
+   public static void main(String[] args)
+   {
+      String exp = "13 15 + 5 * 1 - 5 *";
+      String[] exp2 = exp.split(" ");
+      System.out.println(evalPostfix(exp2));
+   }
+ 
+}
